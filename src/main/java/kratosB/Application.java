649fa00ab5e05d7c85c946dac0ca2e0c96b75ca7
admin <zhiqiang.bao@iqunxing.com>
2018-07-20 16:03:41 +0800

    init

---
+++ src/main/java/kratosB/Application.java
@@ -0,0 +1,23 @@
+package kratosB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @author zhiqiang bao
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
